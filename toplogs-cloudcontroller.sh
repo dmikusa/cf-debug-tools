228c120e91a1fa6b828ec49cf38f682b6d799494
Daniel Mikusa <dmikusa@pivotal.io>
2017-12-13 09:46:17 -0500

    Modified to accept multiple files.  - Adjusted argument parsing so that top count is optional and set with `-t` or `--top`  - files to parse are now set as positional arguments and you can include as many files as you want

--- toplogs-cloudcontroller.sh
+++ toplogs-cloudcontroller.sh
@@ -6,67 +6,85 @@
 #
 set -e  # dont add `-o pipefail`, this will cause false errors
 
-printf "Assumes standard CloudController Nginx access log format:\n\n"
-echo '<client ip> - [<date>:<time>] "<method> <request path> <http version>" <status code> <bytes> "<referrer" "<user agent>" <x-forwarded-for> vcap_request_id:<reqest_id> response_time:<response_time>'
+LOGREGEX='^(.*?) - \[(\d{2})\/(\w{3})\/(\d{4}):(\d{2}):(\d{2}):(\d{2}) (.*?)\] "(.*?) (.*?) (.*?)" (\d+) (\d+) "(.*?)" "(.*?)" (.*?) vcap_request_id:(.*?) response_time:(\d+\.\d+)$'
 
-apacheRx='^(.*?) - \[(\d{2})\/(\w{3})\/(\d{4}):(\d{2}):(\d{2}):(\d{2}) (.*?)\] "(.*?) (.*?) (.*?)" (\d+) (\d+) "(.*?)" "(.*?)" (.*?) vcap_request_id:(.*?) response_time:(\d+\.\d+)$'
+# parse out args
+#  - https://stackoverflow.com/a/14203146/1585136
+TOP=10
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -t|--top)
+        TOP="$2"
+        shift
+        shift
+        ;;
+    *)
+    POSITIONAL+=("$1")
+    shift
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}"
 
-if [ $# -eq 0 ]; then
-    echo "Usage (for top 10):"
-    echo "toplogs-cloudcontroller.sh access.log 10"
+usage () {
+    echo "USAGE:"
+    echo "toplogs-cloudcontroller.sh [-t|--top 10] <file1> <file2> <file3> ..."
+    echo ""
+    echo "    -t|--top - defaults to 10, sets the number of results to return"
+    echo ""
+    echo "NOTES:"
+    echo "  Assumes standard CloudController Nginx access log format:"
+    echo '    <client ip> - [<date>:<time>] "<method> <request path> <http version>" <status code> <bytes> "<referrer" "<user agent>" <x-forwarded-for> vcap_request_id:<reqest_id> response_time:<response_time>'
     exit 1
-fi
-
-if [ -z "$2" ]
-then
-	total=10
-else
-	total=$2
-fi
+}
 
 printHeader () {
     printf "\n--------------------------------------\n"
-    printf "%s" "$1"
+    printf "  %s" "$1"
     printf "\n--------------------------------------\n\n"
 }
 
-printHeader 'Response Codes'
-perl -n -e '/'"$apacheRx"'/ && print $12."\r\n"' "$1" | sort | uniq -c | sort -nr
+main () {
+    printHeader 'Response Codes'
+    perl -n -e '/'"$LOGREGEX"'/ && print $12."\r\n"' <( cat "$@" ) | sort | uniq -c | sort -nr
 
-printHeader 'Request Methods'
-perl -n -e '/'"$apacheRx"'/ && print $9."\r\n"' "$1" | sort | uniq -c | sort -nr
+    printHeader 'Request Methods'
+    perl -n -e '/'"$LOGREGEX"'/ && print $9."\r\n"' <( cat "$@" ) | sort | uniq -c | sort -nr
 
-printHeader 'Top '"$total"' Requests (no query params)'
-perl -n -e '/'"$apacheRx"'/ && print $10."\r\n"' "$1" | cut -d '?' -f 1 | sort | uniq -c | sort -nr | head -n "$total"
+    printHeader 'Top '"$TOP"' Requests (no query params)'
+    perl -n -e '/'"$LOGREGEX"'/ && print $10."\r\n"' <( cat "$@" ) | cut -d '?' -f 1 | sort | uniq -c | sort -nr | head -n "$TOP"
 
-printHeader 'Top '"$total"' Requests (with query params)'
-perl -n -e '/'"$apacheRx"'/ && print $10."\r\n"' "$1" | sort | uniq -c | sort -nr | head -n "$total"
+    printHeader 'Top '"$TOP"' Requests (with query params)'
+    perl -n -e '/'"$LOGREGEX"'/ && print $10."\r\n"' <( cat "$@" ) | sort | uniq -c | sort -nr | head -n "$TOP"
 
-printHeader 'Top '"$total"' User Agents'
-perl -n -e '/'"$apacheRx"'/ && print $15."\r\n"' "$1" | sort | uniq -c | sort -nr | head -n "$total"
+    printHeader 'Top '"$TOP"' User Agents'
+    perl -n -e '/'"$LOGREGEX"'/ && print $15."\r\n"' <( cat "$@" ) | sort | uniq -c | sort -nr | head -n "$TOP"
 
-printHeader 'Top '"$total"' Referrers'
-perl -n -e '/'"$apacheRx"'/ && print $14."\r\n"' "$1" | sort | uniq -c | sort -nr | head -n "$total"
+    printHeader 'Top '"$TOP"' Referrers'
+    perl -n -e '/'"$LOGREGEX"'/ && print $14."\r\n"' <( cat "$@" ) | sort | uniq -c | sort -nr | head -n "$TOP"
 
-printHeader 'Top '"$total"' Forwarded IPs'
-perl -n -e '/'"$apacheRx"'/ && print $16."\r\n"' "$1" | sort | uniq -c | sort -nr | head -n "$total"
+    printHeader 'Top '"$TOP"' Forwarded IPs'
+    perl -n -e '/'"$LOGREGEX"'/ && print $16."\r\n"' <( cat "$@" ) | sort | uniq -c | sort -nr | head -n "$TOP"
 
-printHeader 'Top '"$total"' Direct Client IP'
-perl -n -e '/'"$apacheRx"'/ && print $1."\r\n"' "$1" | sort | uniq -c | sort -nr | head -n "$total"
+    printHeader 'Top '"$TOP"' Direct Client IP'
+    perl -n -e '/'"$LOGREGEX"'/ && print $1."\r\n"' <( cat "$@" ) | sort | uniq -c | sort -nr | head -n "$TOP"
 
-printHeader 'Top '"$total"' Days'
-perl -n -e '/'"$apacheRx"'/ && print $2."/".$3."/".$4."\r\n"' "$1" | sort | uniq -c | sort -nr | head -n "$total"
+    printHeader 'Top '"$TOP"' Days'
+    perl -n -e '/'"$LOGREGEX"'/ && print $2."/".$3."/".$4."\r\n"' <( cat "$@" ) | sort | uniq -c | sort -nr | head -n "$TOP"
 
-printHeader 'Top '"$total"' Hours'
-perl -n -e '/'"$apacheRx"'/ && print $2."/".$3."/".$4." ".$5."\r\n"' "$1" | sort | uniq -c | sort -nr | head -n "$total"
+    printHeader 'Top '"$TOP"' Hours'
+    perl -n -e '/'"$LOGREGEX"'/ && print $2."/".$3."/".$4." ".$5."\r\n"' <( cat "$@" ) | sort | uniq -c | sort -nr | head -n "$TOP"
 
-printHeader 'Top '"$total"' Minutes'
-perl -n -e '/'"$apacheRx"'/ && print $2."/".$3."/".$4." ".$5.":".$6."\r\n"' "$1" | sort | uniq -c | sort -nr | head -n "$total"
+    printHeader 'Top '"$TOP"' Minutes'
+    perl -n -e '/'"$LOGREGEX"'/ && print $2."/".$3."/".$4." ".$5.":".$6."\r\n"' <( cat "$@" ) | sort | uniq -c | sort -nr | head -n "$TOP"
 
-printHeader 'Top '"$total"' Seconds'
-perl -n -e '/'"$apacheRx"'/ && print $2."/".$3."/".$4." ".$5.":".$6.":".$7."\r\n"' "$1" | sort | uniq -c | sort -nr | head -n "$total"
+    printHeader 'Top '"$TOP"' Seconds'
+    perl -n -e '/'"$LOGREGEX"'/ && print $2."/".$3."/".$4." ".$5.":".$6.":".$7."\r\n"' <( cat "$@" ) | sort | uniq -c | sort -nr | head -n "$TOP"
 
-printHeader 'Top '"$total"' Response Times (secs)'
-perl -n -e '/'"$apacheRx"'/ && print $18."\n"' "$1" | xargs printf "%.0f\n" | sort -n | uniq -c | sort -nr | head -n "$total"
+    printHeader 'Top '"$TOP"' Response Times (secs)'
+    perl -n -e '/'"$LOGREGEX"'/ && print $18."\n"' <( cat "$@" ) | xargs printf "%.0f\n" | sort -n | uniq -c | sort -nr | head -n "$TOP"
+}
 
-exit 0
+main "$@"
